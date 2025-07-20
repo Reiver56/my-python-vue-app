523afecc22c7488d0e14f865844ed0b11434da29
Reiver56 <greverion56@gmail.com>
2025-07-21 01:40:45 +0200

    trying something with a new file

---
+++ tmp/nodes/reader.py
@@ -0,0 +1,12 @@
+
+def read_file(file_path: str) -> str:
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return "File not found."
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+print(read_file("tmp/nodes/pipeline.corgi"))
